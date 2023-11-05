d575143e27353ac662c8d79ed81acaf3391e1eec
JaKarski <kacper.karski01@gmail.com>
2023-11-05 17:38:38 +0100

    Add code for models, create samples for tests

---
+++ bezdech_model.py
@@ -0,0 +1,54 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, accuracy_score
+from joblib import dump
+
+# Wczytywanie danych profilu
+def load_data(filename):
+    with open(filename, 'r') as f:
+        sequences = []
+        for line in f.readlines():
+            sequences.append([float(value) for value in line.split(' ')])
+        return np.array(sequences)
+
+
+# Załadowanie danych
+profile_data = load_data('data_categorised/bezdech.txt')
+
+# Przygotowanie etykiet
+# Wszystkie dane z profilu będą miały etykietę 1
+labels = np.ones(len(profile_data))
+
+# Przygotowanie danych i etykiet (przy założeniu, że mamy równą ilość danych "nieprofilowych")
+# Tu symulujemy "negatywne" przypadki jako losowe dane
+# W rzeczywistym przypadku powinieneś użyć rzeczywistych danych, które nie pasują do profilu
+non_profile_data = np.random.rand(len(profile_data), profile_data.shape[1]) # symulacja danych niezwiązanych z profilem
+non_profile_labels = np.zeros(len(non_profile_data)) # etykieta 0 dla niezwiązanych danych
+
+# Łączenie danych
+X = np.vstack((profile_data, non_profile_data))
+y = np.hstack((labels, non_profile_labels))
+
+# Normalizacja danych
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
+# Podział na zestawy treningowe i testowe
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Tworzenie modelu SVM
+clf = svm.SVC(gamma='scale')
+
+# Trenowanie modelu
+clf.fit(X_train, y_train)
+
+# Przewidywanie na zestawie testowym
+y_pred = clf.predict(X_test)
+
+# Ocena modelu
+print(classification_report(y_test, y_pred))
+print('Accuracy:', accuracy_score(y_test, y_pred))
+
+dump(clf, 'modele/bezdech.joblib')
--- data_output/create_output.py
+++ data_output/create_output.py
@@ -2,19 +2,27 @@ from scripts.load_data import load_data, save_data
 from scripts.normalization import normalize
 from joblib import load
 
+#model = 'wdech_wydech'
+model = 'bezdech'
+#model = 'zatrzymanie'
+
+samples = 10
+
 # Wczytanie modelu z pliku
-loaded_clf = load('../modele/wdech_wydech.joblib')
+loaded_clf = load('../modele/'+model+'.joblib')
 
 data = load_data('../data_raw/raw_data.txt')
 data = normalize(data)
-data_output = [[0], [0], [0], [0]]
+data_output = []
+for i in range(samples):
+    data_output.append([0])
 
 
-def devide_segments(dane, data_output, clf, dlugosc_segmentu=5):
+def devide_segments(dane, data_output, clf, dlugosc_segmentu=samples):
     for i in range(len(dane) - dlugosc_segmentu + 1):
         segment = dane[i:i + dlugosc_segmentu]
         data_output.append(clf.predict([segment]))
 
 
 devide_segments(data, data_output, loaded_clf)
-save_data('wdech_wydech.txt', data_output)
+save_data(model+'.txt', data_output)
